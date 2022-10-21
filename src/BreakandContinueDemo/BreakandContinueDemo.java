e23a181ad7ac20f9ce964b06e0aa327f4157ee4d
unknown <engr.mohammadjannatulnayeem@gmail.com>
2022-10-21 20:52:21 +0600

    code uploaded

---
+++ src/BreakandContinueDemo/BreakandContinueDemo.java
@@ -0,0 +1,29 @@
+
+package BreakandContinueDemo;
+
+/**
+ *
+ * @author nayeem
+ */
+public class BreakandContinueDemo {
+   
+    public static void main(String[]args)
+    {
+        for(int x=1;x<=10;x++)
+        {
+          if(x==5)
+          {
+           break;
+          }
+            
+        
+        System.out.println("output is:"+x);
+        
+        }
+    
+    
+    
+    
+    }
+    
+}
---
+++ src/BreakandContinueDemo/BreakandContinueDemo2.java
@@ -0,0 +1,28 @@
+
+package BreakandContinueDemo;
+
+/**
+ *
+ * @author nayeem
+ */
+public class BreakandContinueDemo2 {
+    public static void main(String[]args)
+    {
+    for(int x=1;x<=10;x++)
+    {
+    
+    if( x==5)
+    {
+    continue;
+    }
+    
+    System.out.println("output is:"+x);
+    
+    }
+    
+    
+    
+    
+    }
+    
+}
